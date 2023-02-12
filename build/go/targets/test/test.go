7a17aad695a02969b7e206fc0e3ea73bef6ddf78
da-moon <contact@havi.dev>
2023-02-12 12:31:01 +0000

    docs(build/go/targets/test): `Target` function synopsis

--- build/go/targets/test/test.go
+++ build/go/targets/test/test.go
@@ -5,6 +5,8 @@ import (
 	sh "github.com/magefile/mage/sh"
 )
 
+// Target uses `gorc` to recursively
+// run all project's unit tests.
 func Target() error {
 	args := []string{"test"}
 	if mg.Verbose() {
