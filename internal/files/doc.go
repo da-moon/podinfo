a163f57dfa7edacfb33fcf92a0a81874f715945f
da-moon <contact@havi.dev>
2023-02-12 01:45:10 +0000

    docs(internal/files): synopsis

---
+++ internal/files/doc.go
@@ -0,0 +1,9 @@
+// Package files contains utility functions to make working with files easier
+// and more straight forward
+// - only linux is supported
+// ─── ACKNOWLEDGEMENTS ───────────────────────────────────────────────────────────
+//   - github.com/prometheus/prometheus/tsdb/fileutil
+//   - github.com/VictoriaMetrics/VictoriaMetrics/lib/fs
+//
+// ────────────────────────────────────────────────────────────────────────────────
+package files
