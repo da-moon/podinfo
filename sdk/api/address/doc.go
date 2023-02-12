23ab44b7176bc010e20c25374916ddd2159959a8
da-moon <contact@havi.dev>
2023-02-12 01:34:54 +0000

    docs(sdk/api/address): synopsis

---
+++ sdk/api/address/doc.go
@@ -0,0 +1,6 @@
+// Package address exposes utility
+// functions for working with the tcp/udp addresses.
+//
+// These functions are used by the sdk package to
+// construct and parse addresses.
+package address
