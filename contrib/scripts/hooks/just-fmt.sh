f80992fbee627a72d99b0b6789d74d9088ebeb06
da-moon <contact@havi.dev>
2023-02-12 19:01:30 +0000

    docs(just/bootstrap): `kary-comments` target synopsis

--- contrib/scripts/hooks/just-fmt.sh
+++ contrib/scripts/hooks/just-fmt.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # NOTE: simulate this pre-commit hook script:
 #
-# contrib/scripts/hooks/just-fmt.sh $(git diff --cached --name-only --diff-filter=ACM | grep -E 'Justfile|\.just$')
+# contrib/scripts/hooks/just-fmt.sh $(git diff --cached --name-only --diff-filter=ACMR | grep -E 'Justfile|\.just$')
 to_fmt=()
 for file in "$@"; do
-  just --summary --justfile "${file}" > /dev/null 2>&1
+  just --unstable --summary --justfile "${file}" > /dev/null 2>&1
   if [ $? -eq 1 ]; then
     echo "There are errors in '${file}', please fix them before committing"
     exit 1
@@ -21,6 +21,4 @@ if (( ${#to_fmt[@]} )); then
   done
   echo "Justfile re-formatted, please re-add it to your commit"
   exit 1
-else
-  exit 0 ;
 fi
