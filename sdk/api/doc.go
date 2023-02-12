f657f443d0cc2c02df40274a172efe1c50b3a107
da-moon <contact@havi.dev>
2023-02-12 01:39:45 +0000

    docs(sdk/api): synopsis

---
+++ sdk/api/doc.go
@@ -0,0 +1,8 @@
+// Package api aggregates all
+// libraries that help with setting
+// up a web server.
+// ─── NOTES ──────────────────────────────────────────────────────────────────────
+// - use logrus package (github.com/sirupsen/logrus)
+// for the logs that are getting sent to external vendors
+// ────────────────────────────────────────────────────────────────────────────────
+package api
