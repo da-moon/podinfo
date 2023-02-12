ffb9e6bbbd3ae65e4b4d6c12d6eec8469543a5f0
da-moon <contact@havi.dev>
2023-02-12 01:44:53 +0000

    test(internal/files): init

---
+++ internal/files/file_test.go
@@ -0,0 +1,29 @@
+package files_test
+
+import (
+	"testing"
+
+	files "github.com/da-moon/northern-labs-interview/internal/files"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsTemporaryFileName(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{input: "", expected: false},
+		{input: ".", expected: false},
+		{input: ".tmp", expected: false},
+		{input: "tmp.123", expected: false},
+		{input: ".tmp.123.xx", expected: false},
+		{input: "asdf.sdfds.tmp.dfd", expected: false},
+		{input: "dfd.sdfds.dfds.1232", expected: false},
+		{input: ".tmp.1", expected: true},
+		{input: "asdf.dff.tmp.123", expected: true},
+	}
+	for _, tt := range tests {
+		actual := files.IsTemporaryFileName(tt.input)
+		assert.Equal(t, tt.expected, actual)
+	}
+}
---
+++ internal/files/path_test.go
@@ -0,0 +1,52 @@
+package files_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	files "github.com/da-moon/northern-labs-interview/internal/files"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSubdirGlob(t *testing.T) {
+	assert := assert.New(t)
+	td, err := ioutil.TempDir("", "subdir-glob")
+	assert.NoError(err)
+	assert.NotEmpty(td)
+	defer os.RemoveAll(td)
+
+	err = os.Mkdir(filepath.Join(td, "subdir"), 0755)
+	assert.NoError(err)
+	err = os.Mkdir(filepath.Join(td, "subdir/one"), 0755)
+	assert.NoError(err)
+
+	err = os.Mkdir(filepath.Join(td, "subdir/two"), 0755)
+	assert.NoError(err)
+	expected := filepath.Join(td, "subdir")
+
+	t.Run("march_exact_dir", func(t *testing.T) {
+		actual, err := files.SubdirGlob(td, "subdir")
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.NoError(err)
+		assert.Equal(expected, actual)
+	})
+	t.Run("single_match_wildcard", func(t *testing.T) {
+		actual, err := files.SubdirGlob(td, "*")
+		assert.NoError(err)
+		assert.Equal(expected, actual)
+	})
+	t.Run("multiple_matches", func(t *testing.T) {
+		actual, err := files.SubdirGlob(td, "subdir/*")
+		assert.Error(err)
+		assert.Empty(actual)
+	})
+	t.Run("non_existent", func(t *testing.T) {
+		actual, err := files.SubdirGlob(td, "foo")
+		assert.Error(err)
+		assert.Empty(actual)
+	})
+}
