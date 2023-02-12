b0e08746f124e45f0836475b4f8bff12301ee56c
da-moon <contact@havi.dev>
2023-02-12 01:46:54 +0000

    docs(api/handlers): synopsis

---
+++ api/handlers/doc.go
@@ -0,0 +1,6 @@
+// Package handlers contains functions that
+// setup API endpoint handlers.
+// by convention , every endpoint should be defined
+// in a separate file and have an initialization function that
+// would register itself with the API registry
+package handlers
