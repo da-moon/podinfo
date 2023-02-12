5eb4b1836c69ff2c2cfb8484440c55607794d9d9
da-moon <contact@havi.dev>
2023-02-12 01:32:05 +0000

    feat(sdk/physical/chroot): init

---
+++ sdk/physical/chroot/internal.go
@@ -0,0 +1,28 @@
+package chroot
+
+import (
+	"strings"
+
+	physical "github.com/da-moon/northern-labs-interview/sdk/physical"
+	stacktrace "github.com/palantir/stacktrace"
+)
+
+// sanityCheck is used to perform a sanity check on a key
+func (v *View) sanityCheck(key string) error {
+	if strings.Contains(key, "..") {
+		errMsg := physical.RelativePathErrorMessage
+		err := stacktrace.NewError(errMsg)
+		return err
+	}
+	return nil
+}
+
+// expandKey is used to expand to the full key path with the prefix
+func (v *View) expandKey(suffix string) string {
+	return v.prefix + suffix
+}
+
+// truncateKey is used to remove the prefix of the key
+func (v *View) truncateKey(full string) string {
+	return strings.TrimPrefix(full, v.prefix)
+}
---
+++ sdk/physical/chroot/view.go
@@ -0,0 +1,71 @@
+package chroot
+
+import (
+	"context"
+
+	physical "github.com/da-moon/northern-labs-interview/sdk/physical"
+)
+
+// View represents a prefixed view of a physical backend
+type View struct {
+	backend physical.Backend
+	prefix  string
+}
+
+// New takes an underlying physical backend and returns
+// a view of it that can only operate with the given prefix.
+func New(backend physical.Backend, prefix string) *View {
+	return &View{
+		backend: backend,
+		prefix:  prefix,
+	}
+}
+
+// List the contents of the prefixed view
+func (v *View) List(ctx context.Context, prefix string) ([]string, error) {
+	if err := v.sanityCheck(prefix); err != nil {
+		return nil, err
+	}
+	return v.backend.List(ctx, v.expandKey(prefix))
+}
+
+// Get the key of the prefixed view
+func (v *View) Get(ctx context.Context, key string) (*physical.Entry, error) {
+	if err := v.sanityCheck(key); err != nil {
+		return nil, err
+	}
+	entry, err := v.backend.Get(ctx, v.expandKey(key))
+	if err != nil {
+		return nil, err
+	}
+	if entry == nil {
+		return nil, nil
+	}
+	if entry != nil {
+		entry.Key = v.truncateKey(entry.Key)
+	}
+	return &physical.Entry{
+		Key:   entry.Key,
+		Value: entry.Value,
+	}, nil
+}
+
+// Put the entry into the prefix view
+func (v *View) Put(ctx context.Context, entry *physical.Entry) error {
+	if err := v.sanityCheck(entry.Key); err != nil {
+		return err
+	}
+	nested := &physical.Entry{
+		Key:   v.expandKey(entry.Key),
+		Value: entry.Value,
+	}
+	return v.backend.Put(ctx, nested)
+}
+
+// Delete the entry from the prefix view
+func (v *View) Delete(ctx context.Context, key string) error {
+	if err := v.sanityCheck(key); err != nil {
+		return err
+	}
+	return v.backend.Delete(ctx, v.expandKey(key))
+}
