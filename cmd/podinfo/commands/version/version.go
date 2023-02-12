df8d8d1a1bdd417ad8ca71a2f2951dbfcc57e06a
da-moon <contact@havi.dev>
2023-02-12 01:30:33 +0000

    feat(cmd/podinfo/commands/version): init

---
+++ cmd/podinfo/commands/version/version.go
@@ -0,0 +1,42 @@
+package version
+
+import (
+	"strings"
+
+	buildver "github.com/da-moon/northern-labs-interview/internal/version"
+	cli "github.com/mitchellh/cli"
+)
+
+func New(ui cli.Ui) *cmd {
+	c := &cmd{
+		UI: ui,
+	}
+	return c
+}
+
+type cmd struct {
+	UI       cli.Ui
+	help     string
+	synopsis string
+}
+
+func (c *cmd) Run(_ []string) int {
+	build := buildver.New()
+	c.UI.Output(build.ToString())
+	return 0
+}
+func (c *cmd) Synopsis() string {
+	return strings.TrimSpace(c.synopsis)
+}
+
+func (c *cmd) Help() string {
+	return strings.TrimSpace(c.help)
+}
+
+const synopsis = "podinfo immutable build information ."
+
+const help = `
+Usage: podinfo version
+
+returns current release buildver.
+`
