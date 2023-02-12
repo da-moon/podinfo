9706c76925065ed8bd02bb80ea84b755e66e1332
da-moon <contact@havi.dev>
2023-02-12 01:39:33 +0000

    docs(sdk/physical): synopsis

---
+++ sdk/physical/doc.go
@@ -0,0 +1,3 @@
+// Package physical contains some common widely used helper implementation
+// of physical interface and some shared structs used in implementation of Physical Interface
+package physical
