d3233c89166be6061014360a8e0ba507a8743c01
da-moon <contact@havi.dev>
2023-02-12 01:46:31 +0000

    docs(api/middlewares): synopsis

---
+++ api/middlewares/doc.go
@@ -0,0 +1,5 @@
+// Package middlewares implements a collection
+// of useful and commonly used middlewares
+// for http servers that are based
+// on go's standard net/http library
+package middlewares
