28764fbf86eb372be8320573d2e839db902dfa33
da-moon <contact@havi.dev>
2023-02-12 20:19:21 +0000

    build(just/git): `git-fetch` target implementation

--- contrib/scripts/hooks/just-fmt.sh
+++ contrib/scripts/hooks/just-fmt.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 # NOTE: simulate this pre-commit hook script:
 #
-# contrib/scripts/hooks/just-fmt.sh $(git diff --cached --name-only --diff-filter=ACMR | grep -E '\.just$')
+# contrib/scripts/hooks/just-fmt.sh
 #
 # create justfile
-cat <<'EOF' > Justfile.commit
+WD="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../" && pwd)"
+pushd "$WD" >/dev/null 2>&1
+rm -f Justfile.commit
+cat << 'EOF' > Justfile.commit
 # !/usr/bin/env -S just --justfile
 # vim: filetype=just tabstop=2 shiftwidth=2 softtabstop=2 expandtab:
 # ────────────────────────────────────────────────────────────────────────────────
 EOF
-for file in "$@"; do
-sed \
-  -e '/env -S just --justfile/d' \
-  -e '/# vim: /d' "${file}" \
-  -e '/# ────/d' | tee -a Justfile.commit > /dev/null
-done
+while read file ; do
+  sed \
+    -e '/env -S just --justfile/d' \
+    -e '/# vim: /d' \
+    -e '/# ────/d' \
+    "${file}" \
+  | tee -a Justfile.commit > /dev/null
+done < <(find . -name '*.just' -type f )
 just --unstable --summary --justfile Justfile.commit > /dev/null 2>&1
 if [ $? -eq 1 ]; then
   echo "There are errors in concatenated Justfile, please fix them before committing."
   echo "You can run the following to see error messages:"
   echo ""
-  echo "just --unstable --summary --justfile Justfile.commit Justfile.commit"
+  echo "just --unstable --summary --justfile Justfile.commit"
   exit 1
 fi
 just --unstable --fmt --justfile Justfile.commit
@@ -36,4 +41,6 @@ if [ $? -eq 1 ]; then
   mv Justfile.commit Justfile
   exit 1
 fi
+rm -f Justfile.commit
 exit 0
+popd >/dev/null 2>&1
