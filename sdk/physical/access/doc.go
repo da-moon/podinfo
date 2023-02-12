cea7bee6e30457855359ad2bd6254f9049e4d0b1
da-moon <contact@havi.dev>
2023-02-12 01:32:41 +0000

    docs(sdk/physical/access): synopsis

---
+++ sdk/physical/access/doc.go
@@ -0,0 +1,11 @@
+// Package access allows a service to expose its physical
+// storage operations through PhysicalAccess() while
+// restricting the ability to modify Service.physical itself.
+// It should be used when returning a `physical` field
+// of a parent to wrap the field for security purposes
+package access
+
+import "github.com/da-moon/northern-labs-interview/sdk/physical"
+
+// Verify PhysicalAccess satisfies the correct interfaces
+var _ physical.Backend = (*PhysicalAccess)(nil)
