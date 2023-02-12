4b31bb451e2fe001ab6e3974cc91c078949c5c59
da-moon <contact@havi.dev>
2023-02-12 01:32:11 +0000

    docs(sdk/physical/chroot): synopsis

---
+++ sdk/physical/chroot/doc.go
@@ -0,0 +1,10 @@
+// Package chroot helps with representig
+// a prefixed view ( chroot ) of a physical backend
+// this ensures program or users cannot access data they are
+// not supposed to access
+package chroot
+
+import "github.com/da-moon/northern-labs-interview/sdk/physical"
+
+// Verify View satisfies the correct interfaces
+var _ physical.Backend = (*View)(nil)
