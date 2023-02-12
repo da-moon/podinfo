70446b16d0c11691fc882f237a3a03c6d0aea447
da-moon <contact@havi.dev>
2023-02-12 01:31:40 +0000

    docs(sdk/physical/error-injector): synopsis

---
+++ sdk/physical/error-injector/doc.go
@@ -0,0 +1,10 @@
+// Package errorinjector helps with error injection into
+// underlying physical requests . It also supports transactional
+// physical requests
+package errorinjector
+
+import "github.com/da-moon/northern-labs-interview/sdk/physical"
+
+// Verify ErrorInjector satisfies the correct interfaces
+var _ physical.Backend = (*ErrorInjector)(nil)
+var _ physical.Transactional = (*TransactionalErrorInjector)(nil)
