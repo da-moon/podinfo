2a1ffd489a71459e35bbc6dbcf32111b09c127e1
da-moon <contact@havi.dev>
2023-02-12 12:19:25 +0000

    docs(build/go/git): package synopsis

---
+++ build/go/git/doc.go
@@ -0,0 +1,5 @@
+// Package git exposes some helper API that
+// uses native Go implementation of git to extract
+// information about the current git repository
+// it is inject version versioning information during the build
+package git
