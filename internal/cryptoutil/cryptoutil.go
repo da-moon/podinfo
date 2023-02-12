34a459788c8b449d7957cb11fd3223fbab4168be
da-moon <contact@havi.dev>
2023-02-12 01:45:30 +0000

    feat(internal/cryptoutil): init

---
+++ internal/cryptoutil/cryptoutil.go
@@ -0,0 +1,9 @@
+package cryptoutil
+
+import "golang.org/x/crypto/blake2b"
+
+func Blake2b256Hash(key string) []byte {
+	hf, _ := blake2b.New256(nil)
+	hf.Write([]byte(key)) //nolint:errcheck
+	return hf.Sum(nil)
+}
