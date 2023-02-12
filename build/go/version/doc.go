265da8cbbb7155de126bb584cd97f2063bd92135
da-moon <contact@havi.dev>
2023-02-12 12:15:13 +0000

    docs(build/go/version): package synopsis

---
+++ build/go/version/doc.go
@@ -0,0 +1,5 @@
+// Package version exposes public variables
+// and helper functions that enables embedding versioning
+// information into the binary at build time. The helper
+// functions help with showing those info to the user
+package version
