2ac0c5e7abcde72f3e29335ce17c2311b96cbcf7
da-moon <contact@havi.dev>
2023-02-13 06:56:43 -0500

    docs(api/handlers/liveness): `Status` enum

--- api/handlers/liveness/handler.go
+++ api/handlers/liveness/handler.go
@@ -1,7 +1,10 @@
 package liveness
 
+// Status is an enum representing accepted status values for
+// this request handler
 type Status string
 
 const (
+	// OK represents the OK response
 	OK Status = "OK"
 )
