133a5a8d61214cd437ab47431b7635e034462c4b
da-moon <contact@havi.dev>
2023-02-13 07:24:03 -0500

    feat(api/handlers/liveness): `Handler` struct

--- api/handlers/liveness/handler.go
+++ api/handlers/liveness/handler.go
@@ -1,5 +1,11 @@
 package liveness
 
+import (
+	"net/http"
+
+	response "github.com/da-moon/northern-labs-interview/sdk/api/response"
+)
+
 const (
 	// Name stores a human friendly, param-cased
 	// unique identifier name for this endpoint
@@ -7,3 +13,13 @@ const (
 	// Path represents the URI path of this endpoint
 	Path = "/healthz"
 )
+
+// handler function handles incoming HTTP request
+// it satisfies golang's stdlib
+// request handler interface (http.HandlerFunc)
+var handler = func(w http.ResponseWriter, r *http.Request) { //nolint:gochecknoglobals //this function is scoped only to this package
+	response.WriteSuccessfulJSON(w, r, &Response{
+		Status: "OK",
+	})
+	return
+}
---
+++ api/handlers/liveness/init.go
@@ -0,0 +1,9 @@
+package liveness
+
+import (
+	logger "github.com/da-moon/northern-labs-interview/internal/logger"
+)
+
+type Handler struct {
+	log *logger.WrappedLogger
+}
