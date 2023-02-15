229f6ef6ee3ba17e7ad9d98e48fd30f6651484f0
da-moon <contact@havi.dev>
2023-02-14 20:08:19 -0500

    feat(api/handlers/readiness): `handler.Register()` implementation

--- api/handlers/readiness/init.go
+++ api/handlers/readiness/init.go
@@ -1,5 +1,14 @@
 package readiness
 
+import (
+	"net/http"
+
+	middlewares "github.com/da-moon/northern-labs-interview/api/middlewares"
+	registry "github.com/da-moon/northern-labs-interview/api/registry"
+	route "github.com/da-moon/northern-labs-interview/sdk/api/route"
+	stacktrace "github.com/palantir/stacktrace"
+)
+
 const (
 	// Name stores a human friendly, param-cased
 	// unique identifier name for this endpoint
@@ -21,4 +30,25 @@ var (
 
 func init() { // nolint:gochecknoinits // this is to ensure router starts with OK state
 	Router.SetStatus(OK)
+}
+
+// Register this request handler in the central
+// api handlers registry. It also sets the appropriate
+// middlewares.
+// this function should be called when server is getting ready.
+func (h *handler) Register() error {
+	l := h.GetLogger()
+	if l == nil {
+		return stacktrace.NewError("logger is nil")
+	}
+	r := route.New()
+	r.SetName(Name)
+	r.SetPath(Path)
+	r.SetMethod(http.MethodGet)
+	r.SetHandlerFunc(HandlerFn)
+	l.Info("Adding log middleware for '%s' handler", Name)
+	r.AppendMiddleware(middlewares.Log(l))
+	r.AppendMiddleware(middlewares.Metrics(Path, l))
+	registry.Register(APIGroup, *r)
+	return nil
 }
