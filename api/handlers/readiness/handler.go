a34f22d88cf9cbc2affdfdfb2a5fe92bf5a76d72
da-moon <contact@havi.dev>
2023-02-14 20:03:20 -0500

    feat(api/handlers/readiness): `handler.SetLogger()` implementation

--- api/handlers/readiness/handler.go
+++ api/handlers/readiness/handler.go
@@ -7,6 +7,7 @@ import (
 	logger "github.com/da-moon/northern-labs-interview/internal/logger"
 )
 
+// handler struct encapsulates the state this API endpoint
 // handler needs
 type handler struct {
 	// mutex for guard shared state
@@ -15,4 +16,10 @@ type handler struct {
 	log *logger.WrappedLogger
 	// status represents server readiness status
 	status atomic.Bool
+}
+
+func (h *handler) SetLogger(l *logger.WrappedLogger) {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	h.log = l
 }
