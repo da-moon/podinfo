e260245191ed449721c2fcf383e1d0ab05c240f7
da-moon <contact@havi.dev>
2023-02-14 20:35:05 -0500

    docs(api/handlers/delay): package synopsis

---
+++ api/handlers/delay/doc.go
@@ -0,0 +1,4 @@
+// Package delay holds request handler takes simulates running job
+// by delaying the response for a specified duration passed in as
+// part of the request URI : /delay/{seconds}
+package delay
