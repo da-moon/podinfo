019355e452139b0b5b1cd7bf91feada7f12c70e2
da-moon <contact@havi.dev>
2023-02-14 20:31:14 -0500

    feat(api/handlers/headers): `response` type

---
+++ api/handlers/headers/response.go
@@ -0,0 +1,6 @@
+package headers
+
+// Response type represents this endpoints JSON
+// Response which is a map strings to an array of strings
+// it matches stdlib's httpp.Header type
+type Response map[string][]string
