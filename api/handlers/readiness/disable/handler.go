5e8ae1e07e0756e68fd40b141b5fb003858c5ec6
da-moon <contact@havi.dev>
2023-02-14 20:13:35 -0500

    docs(api/handlers/readiness/disable): `handler` struct synopsis

--- api/handlers/readiness/disable/handler.go
+++ api/handlers/readiness/disable/handler.go
@@ -6,6 +6,8 @@ import (
 	logger "github.com/da-moon/northern-labs-interview/internal/logger"
 )
 
+// handler struct encapsulates the state this API endpoint
+// handler needs
 type handler struct {
 	// mutex for guard shared state
 	mutex sync.RWMutex
