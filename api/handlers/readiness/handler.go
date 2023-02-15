fa4e4e0ca8e456eb05236a0a34ed3d888b22634a
da-moon <contact@havi.dev>
2023-02-14 20:06:30 -0500

    feat(api/handlers/readiness): `HandlerFn` implementation

--- api/handlers/readiness/handler.go
+++ api/handlers/readiness/handler.go
@@ -1,10 +1,15 @@
 package readiness
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
 	"sync"
 	"sync/atomic"
 
 	logger "github.com/da-moon/northern-labs-interview/internal/logger"
+	response "github.com/da-moon/northern-labs-interview/sdk/api/response"
+	stacktrace "github.com/palantir/stacktrace"
 )
 
 // handler struct encapsulates the state this API endpoint
@@ -48,4 +53,50 @@ func (h *handler) GetStatus() Status {
 		return OK
 	}
 	return Unavailable
+}
+
+var HandlerFn = func(w http.ResponseWriter, r *http.Request) { //nolint:gochecknoglobals //this function is scoped only to this package
+	s := Router.GetStatus()
+	code := http.StatusOK
+	body := &Response{
+		Status: s.String(),
+	}
+	loggerfn := func(rr *http.Request, data interface{}) func() {
+		return func() {
+			// NOTE: this is just to stay safe from nil pointer dereference
+			if data != nil {
+				response.LogSuccessfulResponse(rr, data)
+			}
+			return
+		}
+	}
+	if s != OK {
+		code = http.StatusServiceUnavailable
+		loggerfn = func(rr *http.Request, data interface{}) func() {
+			return func() {
+				// NOTE: this is just to stay safe from nil pointer dereference
+				if data != nil {
+					compact := &bytes.Buffer{}
+					// NOTE: this is just to stay safe from nil pointer dereference
+					resp, _ := json.Marshal(data) //nolint:gosec // failure here does not matter
+					if resp != nil {
+						json.Compact(compact, resp) //nolint:gosec // failure here does not matter
+						// msg := fmt.Sprintf("%s : %s", Name, Path)
+						msg := Name
+						err := stacktrace.NewErrorWithCode(stacktrace.ErrorCode(code), compact.String())
+						response.LogErrorResponse(rr, err, msg)
+					}
+				}
+				return
+			}
+		}
+	}
+	defer loggerfn(r, body)()
+	response.WriteJSON(
+		w,
+		r,
+		code,
+		make(map[string]string, 0),
+		body,
+	)
 }
